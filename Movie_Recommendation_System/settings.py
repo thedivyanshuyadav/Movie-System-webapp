94635b1b02108b5951ca597b5280c7c33d610ef4
Divyanshu Yadav <thedivyanshuyadav@gmail.com>
2021-05-03 18:55:58 +0530

    v2

--- Movie_Recommendation_System/settings.py
+++ Movie_Recommendation_System/settings.py
@@ -118,14 +118,14 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = True    
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_URL = '/MovieRecommenderApp/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'MovieRecommenderApp/static')
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
